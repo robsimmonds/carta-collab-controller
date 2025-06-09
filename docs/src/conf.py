d337885d51f47ecddc0a22c06d4cfab822c3ac48
Adrianna Pińska <adrianna.pinska@gmail.com>
2025-06-09 09:55:54 +0200

    Use a custom pygments lexer and extend the default sphinx pygments style to enable ANSI colours in controller test output in docs (#231)

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -4,17 +4,17 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import json
+import os
+import sys
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import json
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -47,6 +47,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Custom style which extends the default Pygments style set by the RTD theme
+# with ANSI colour codes from the pygments ANSI extension
+
+pygments_style = 'style.DefaultAnsiStyle'
 
 # -- Options for HTML output -------------------------------------------------
 
